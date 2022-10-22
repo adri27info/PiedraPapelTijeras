c018bad6cce56623ec1b41f11278cc40ce17f24c
Adrian <adri27informatica@gmail.com>
2022-10-22 02:04:28 +0200

    Aplicacion PiedraPapelTijeras

---
+++ README.md
@@ -0,0 +1,37 @@
+# PiedraPapelTijera
+
+This is a js project based on rock, paper, scissors game.
+
+![Image text](images/app-preview.png)
+
+## Table of contents
+
+- [Description](#description)
+- [Technologies](#technologies)
+- [Installation](#installation)
+
+## Description
+
+This is a js project based on rock, paper, scissors game. The player will be able to select any option manually and the machine will select another option but randomly.
+
+The results will be saved in localStorage so that the player knows at all times the wins and losses he has against the machine.
+
+## Technologies
+
+The project has been created with:
+
+- HTML
+- CSS
+- JS
+
+## Installation
+
+To run this project, we need the following steps:
+
+- Disable the option to delete cookies and data from the site when the browser is closed, so that the information is displayed in the application correctly. This option will be found in your browser settings.
+
+- Download the Live Server extension, which is found in the vscode editor
+
+- Once the extension is downloaded, run the extension and everything will work correctly
+
+- If you get an error, try running the application normally using the index.html file
---
+++ js/index.js
@@ -0,0 +1,145 @@
+const contenedoresJuego = document.querySelectorAll("#juego div");
+const reglasWin = {
+  piedra: "tijeras",
+  papel: "piedra",
+  tijeras: "papel",
+};
+const tituloGanador = document.getElementById("titulo_ganador");
+const btnReiniciar = document.getElementById("btn_reiniciar");
+let textoVictorias = document.getElementById("texto_victorias");
+let textoDerrotas = document.getElementById("texto_derrotas");
+let db = window.localStorage;
+let keysLocalStorage;
+
+btnReiniciar.addEventListener("click", reiniciarJuego);
+
+contenedoresJuego.forEach((element) => {
+  element.addEventListener("click", eleccionPlayer);
+});
+
+function eleccionPlayer(e) {
+  let item = document.getElementById(e.target.id);
+  item.classList.add("player_seleccion");
+  desactivarClickItems();
+}
+
+function desactivarClickItems() {
+  contenedoresJuego.forEach((element) => {
+    element.removeEventListener("click", eleccionPlayer);
+  });
+  comprobarContenedoresNoElegidos();
+}
+
+function comprobarContenedoresNoElegidos() {
+  let arrayItemsNoSeleccionados = [];
+  contenedoresJuego.forEach((element, index) => {
+    if (!element.childNodes[1].classList.contains("player_seleccion")) {
+      arrayItemsNoSeleccionados.push({
+        indice: index,
+      });
+    }
+  });
+  generarItemAleatorio(arrayItemsNoSeleccionados);
+}
+
+function generarItemAleatorio(array) {
+  let indices = [];
+  array.forEach((element) => {
+    indices.push(element.indice);
+  });
+  let max = Math.max(...indices),
+    min = Math.min(...indices),
+    numAleatorio = -1;
+  while (numAleatorio !== max && numAleatorio !== min) {
+    numAleatorio = Math.floor(Math.random() * (max - min + 1) + min);
+  }
+  eleccionMaquina(numAleatorio);
+}
+
+function eleccionMaquina(num) {
+  contenedoresJuego.forEach((element, index) => {
+    if (index === num) {
+      element.childNodes[1].classList.add("maquina_seleccion");
+    }
+  });
+  comprobarGanador();
+}
+
+function comprobarGanador() {
+  let itemPlayerSeleccionado = document
+    .querySelector(".player_seleccion")
+    .id.replace("item_", "");
+  let itemMaquinaSeleccionado = document
+    .querySelector(".maquina_seleccion")
+    .id.replace("item_", "");
+
+  for (let key in reglasWin) {
+    if (reglasWin[key] === reglasWin[itemPlayerSeleccionado]) {
+      if (itemPlayerSeleccionado === reglasWin[itemMaquinaSeleccionado]) {
+        actualizarValores("maquina");
+      } else {
+        actualizarValores("player");
+      }
+    }
+  }
+}
+
+function actualizarValores(ganador) {
+  keysLocalStorage = Object.keys(db);
+  for (let index = 0; index < keysLocalStorage.length; index++) {
+    let obj = JSON.parse(db.getItem(keysLocalStorage[index]));
+    if (obj.nombre === ganador) {
+      obj.victorias = obj.victorias + 1;
+      db.setItem(obj.nombre, JSON.stringify(obj));
+      mostrarMensajeGanador(ganador);
+    }
+  }
+}
+
+function mostrarMensajeGanador(ganador) {
+  imprimirValoresActuales();
+  tituloGanador.textContent = "Ganador: " + ganador;
+  document.querySelector("." + ganador + "_seleccion").classList.add("winner");
+  btnReiniciar.classList.remove("ocultar");
+}
+
+function reiniciarJuego() {
+  window.location.reload();
+}
+
+function almacenarValoresIniciales() {
+  let objetoPlayer = {
+    nombre: "player",
+    victorias: 0,
+  };
+  let objetoMaquina = {
+    nombre: "maquina",
+    victorias: 0,
+  };
+  guardarValoresIniciales(objetoPlayer, objetoMaquina);
+}
+
+function guardarValoresIniciales(objetoPlayer, objetoMaquina) {
+  if (
+    JSON.parse(db.getItem(objetoPlayer.nombre)) === null &&
+    JSON.parse(db.getItem(objetoMaquina.nombre)) === null
+  ) {
+    db.setItem(objetoPlayer.nombre, JSON.stringify(objetoPlayer));
+    db.setItem(objetoMaquina.nombre, JSON.stringify(objetoMaquina));
+  }
+  imprimirValoresActuales();
+}
+
+function imprimirValoresActuales() {
+  keysLocalStorage = Object.keys(db);
+  for (let index = 0; index < keysLocalStorage.length; index++) {
+    let obj = JSON.parse(db.getItem(keysLocalStorage[index]));
+    if (obj.nombre === "player") {
+      textoVictorias.textContent = obj.victorias;
+    } else if (obj.nombre === "maquina") {
+      textoDerrotas.textContent = obj.victorias;
+    }
+  }
+}
+
+almacenarValoresIniciales();
